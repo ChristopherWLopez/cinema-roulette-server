e7b5d34007a871daa5a63d38218c653c0e5b3752
christopher lopez <christopherwlopez89@gmail.com>
2024-01-27 12:25:35 -0800

    routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -34,3 +34,54 @@ async function getUser(req, res, next){
         res.json(rawUser);
     }
 }
+
+async function deleteUser(req, res, next){
+    const id = req.params.id;
+    const user = await User.findOne({ where: { id:id } });
+    if(user === null){
+        next();
+    } else {
+        await user.destroy();
+        res.json({});
+    }
+}
+
+async function createUser(req, res){
+    const username = req.body.username;
+    const birthday = Date.parse(req.body.birthday);
+    const user = await User.create({
+        username,
+        birthday,
+    });
+
+    const hobbies = req.body.hobbies ?? [];
+    for(const name of hobbies){
+        await user.createHobby({ name });
+    }
+    res.json(user);
+}
+
+async function updateUser(req, res, next){
+    const id = req.params.id;
+    let user = await User.findOne({ where: {id:id}});
+    if(user==null){
+        next();
+    } else {
+        const username = req.body.username;
+        const birthday = Date.parse(
+            req.body.birthday ?? user.birthday.toISOString()
+        );
+        let updateUser = {
+            username,
+            birthday,
+        };
+
+        user = await user.update(updateUser);
+
+        res.json(user);
+    }
+}
+
+module.exports = {
+    userRoutes,
+}
